c0d22f80af97b3bf43b94f8a014656aec3581d70
Ayush Das <54628493+Ayush6602@users.noreply.github.com>
2021-01-02 18:59:33 +0530

    Add best5 from genre, api response

--- slgwebapp/api/urls.py
+++ slgwebapp/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path(route='search', view=views.search, name="api_search")
+	path(route='search', view=views.search, name="api_search"),
+	path(route='best5', view=views.best5, name="api_best5")
 ]
