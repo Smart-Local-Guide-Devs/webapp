01105913c6fc2f7c0352941d8465d14c4a750a9d
Ayush Das <54628493+Ayush6602@users.noreply.github.com>
2021-01-02 18:59:33 +0530

    Add best5 from genre, api response

--- slgwebapp/api/views.py
+++ slgwebapp/api/views.py
@@ -1,3 +1,4 @@
+from django.db.models import fields
 from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from .models import App
@@ -7,3 +8,12 @@ def search(request):
 	app_name = request.GET['app_name']
 	app = App.objects.get(app_name=app_name)
 	return JsonResponse(model_to_dict(app))
+
+def best5(request):
+	genre = request.GET['genre']
+	apps = App.objects.filter(genre=genre).order_by('avg_rating')
+	res = {}
+	for i, app in enumerate(apps.values()[:5]):
+		res[i] = app
+	return JsonResponse(res)
+
