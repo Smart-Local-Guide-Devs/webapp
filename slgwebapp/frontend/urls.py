81bf98676b390d899562f72263e0532c3f5ac5ad
Mercury_80 <harsh.gaud18@gmail.com>
2021-01-14 01:33:59 +0530

    Signup Page Working , Error in Login

--- slgwebapp/frontend/urls.py
+++ slgwebapp/frontend/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path(route='', view=views.index, name="index"),
 	path(route='product', view=views.product, name="product"),
 	path(route='review', view=views.review_form, name="review_form"),
-	path(route='login', view=views.login, name="login"),
+	path(route='signin', view=views.signin, name="signin"),
 	path(route='signup', view=views.signup, name="signup"),
 	path(route='search', view=views.search, name="front_search")
 ]
