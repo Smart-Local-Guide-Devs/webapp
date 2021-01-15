4e3d26950c3ee716e9bf265e0f4306bd7578057e
Mercury_80 <harsh.gaud18@gmail.com>
2021-01-15 23:43:01 +0530

    Changed login.html to sign.html , Signin and Signup working properly !

--- slgwebapp/frontend/views.py
+++ slgwebapp/frontend/views.py
@@ -34,12 +34,14 @@ def review_form(request):
 
 def signin(request):
 	if request.method == 'POST':
-		username=request.POST.get('username')
-		password=request.POST.get('password')
+		username=request.POST.get('user')
+		password=request.POST.get('pass')
 		user = authenticate(request,username=username,password=password)
 		if user is not None:
 			login(request,user)
 			return redirect('index')
+		else:
+			messages.info(request, 'Username or Password is Incorrect')	
 	context = {}
 	return render(request, 'signin.html',context)		
 	
