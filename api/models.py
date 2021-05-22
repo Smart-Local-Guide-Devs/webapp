79c3b01e3611861d7b2834550a8015ca89f0c8c6
Mercury_80 <harsh.gaud18@gmail.com>
2021-05-23 00:21:15 +0530

    Slg User Model Added

--- api/models.py
+++ api/models.py
@@ -73,3 +73,14 @@ class Visitor(models.Model):
 
     def __str__(self):
         return self.visitor
+
+class SLG_User(models.Model):
+	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+	name = models.CharField(max_length=200, null=True)
+	phone = models.CharField(max_length=200, null=True)
+	email = models.CharField(max_length=200, null=True)
+	profile_pic = models.ImageField(null=True, blank=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+	def __str__(self):
+		return self.name
