50b6cc5ffdf35ab5ee1575b31e0d61b95b12c4c5
Atik <atiknageshwar07@gmail.com>
2021-06-04 16:01:31 +0530

    modified data.py for DataFrames

--- api/data.py
+++ api/data.py
@@ -1,7 +1,18 @@
 import celery
-from pandas.core.frame import DataFrame
+import pandas as pd
+from .models.py import App, Genre
 
 @periodic_task(run_every=(crontab(day='*/1')), name="update_dataframes", ignore_result=True)
 def update_dataframes():
     pass 
 
+class VeryBigData:
+
+    df_similar_apps =  pd.DataFrame(
+        {
+            "APP_ID":[App.app_id],
+            "APP_NAME":[App.app_name],
+            "APP_DESCRIPTION":[App.app_summary]
+        })
+
+    top_apps = 
