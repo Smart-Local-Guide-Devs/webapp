2d7d48c75db9033d74ba3068440ddd16317930f7
Ayush Das <ayushdas6602@gmail.com>
2021-12-24 01:33:22 +0530

    added theme toggle

--- frontend/static/js/basePage.js
+++ frontend/static/js/basePage.js
@@ -1,3 +1,5 @@
+// utility functions
+
 function makeFormAsync(form, btn, callBack) {
 	form.addEventListener("submit", async function (e) {
 		e.preventDefault();
@@ -20,6 +22,26 @@ function makeFormAsync(form, btn, callBack) {
 		return false;
 	});
 }
+
+// theme change logic
+
+const userThemeBtn = document.querySelector("#userThemeBtn");
+
+function toogleTheme() {
+	const isDark = document.body.classList.toggle("dark-mode");
+	localStorage.setItem("slgIsDark", isDark);
+	userThemeBtn.innerHTML = isDark ? "Dark Mode" : "Light Mode";
+	userThemeBtn.previousElementSibling.classList.toggle("fa-sun-o");
+	userThemeBtn.previousElementSibling.classList.toggle("fa-moon-o");
+}
+
+if (localStorage.getItem("slgIsDark") == "true") {
+	toogleTheme();
+}
+
+userThemeBtn.addEventListener("click", toogleTheme);
+
+// state and city drop down logic
 
 const stateSelect = document.querySelector("#stateSelect");
 const citySelect = document.querySelector("#citySelect");
@@ -49,40 +71,43 @@ stateSelect.addEventListener("change", () => {
 });
 
 citySelect.addEventListener("change", () => {
-	sessionStorage.setItem("locationState", stateSelect.value)
-	sessionStorage.setItem("locationCity", citySelect.value)
-	let appInfoPage = document.querySelector("#stateSelectAppInfo")
+	sessionStorage.setItem("locationState", stateSelect.value);
+	sessionStorage.setItem("locationCity", citySelect.value);
+	let appInfoPage = document.querySelector("#stateSelectAppInfo");
 	if (appInfoPage != null) {
-		stateSelectAppInfo.value = stateSelect.value
-		let changeEvent = new Event("change")
-		stateSelectAppInfo.dispatchEvent(changeEvent)
-		citySelectAppInfo.value = citySelect.value
+		stateSelectAppInfo.value = stateSelect.value;
+		let changeEvent = new Event("change");
+		stateSelectAppInfo.dispatchEvent(changeEvent);
+		citySelectAppInfo.value = citySelect.value;
 	}
-})
+});
+
+// user location fetch logic
 
 async function getLocation() {
-	const res = await fetch("https://geolocation-db.com/json/")
-	return res.json()
+	const res = await fetch("https://geolocation-db.com/json/");
+	return res.json();
 }
 
 function setLocation(dict) {
-	stateSelect.value = dict['state']
-	let changeEvent = new Event("change")
-	stateSelect.dispatchEvent(changeEvent)
-	citySelect.value = dict['city']
-	citySelect.dispatchEvent(changeEvent)
+	stateSelect.value = dict["state"];
+	let changeEvent = new Event("change");
+	stateSelect.dispatchEvent(changeEvent);
+	citySelect.value = dict["city"];
+	citySelect.dispatchEvent(changeEvent);
 }
 
 if (sessionStorage.getItem("locationState") != null) {
-	stateSelect.value = sessionStorage.getItem("locationState")
-	let changeEvent = new Event("change")
-	stateSelect.dispatchEvent(changeEvent)
-	citySelect.value = sessionStorage.getItem("locationCity")
-}
-else {
-	getLocation().then(dict => {
-		setLocation(dict)
-	}).catch(err => {
-		console.log(err)
-	})
+	stateSelect.value = sessionStorage.getItem("locationState");
+	let changeEvent = new Event("change");
+	stateSelect.dispatchEvent(changeEvent);
+	citySelect.value = sessionStorage.getItem("locationCity");
+} else {
+	getLocation()
+		.then((dict) => {
+			setLocation(dict);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 }
